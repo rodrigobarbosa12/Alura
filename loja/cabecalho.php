c71839a97c1c98432cb743429c3bc32e78bd2a09
rodrigobarbosa12 <barbosa@maxscalla.com.br>
2018-03-30 18:14:03 -0300

    30-03

--- loja/cabecalho.php
+++ loja/cabecalho.php
@@ -1,6 +1,24 @@
 <?php
 error_reporting(E_ALL ^ E_NOTICE);
-require_once("mostra-alerta.php"); ?>
+require_once("mostra-alerta.php"); 
+
+function carregaClasse($nomeDaClasse) {
+    require_once("class/".$nomeDaClasse.".php");
+}
+
+spl_autoload_register("carregaClasse");
+
+
+
+//spl_autoload_register(	
+	
+//	function($nomeDaClasse) {
+//  require_once("class/".$nomeDaClasse.".php");
+// });
+
+
+
+?>
 <html>
 <head>
 	<meta charset="utf-8">
