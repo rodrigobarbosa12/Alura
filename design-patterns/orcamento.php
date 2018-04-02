0b04177c72a4be45cb97a14239dc79e2df33d7ae
Rodrigo Barbosa <barbosa@maxscalla.com.br>
2018-04-02 18:28:47 -0300

    Design-patterns

---
+++ design-patterns/orcamento.php
@@ -0,0 +1,16 @@
+<?php
+
+class Orcamento {
+
+    private $valor;
+
+    function __construct($valor){
+        $this->valor = $valor;
+    }
+
+    public function getValor(){
+        return $this->valor;
+
+    }
+
+}
