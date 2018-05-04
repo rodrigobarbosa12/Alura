ac8bb6d8b9bad46eae6f0329ae358250671cf8f6
Rodrigo Barbosa <barbosa@maxscalla.com.br>
2018-05-04 18:12:20 -0300

    E se alguém criasse nossos proxies? O Padrão de Projeto Factory

--- JavaScript/aluraframe/client/js/app/controllers/NegociacaoController.js
+++ JavaScript/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -6,25 +6,28 @@ class NegociacaoController{
             this._inputData = $("#data");
             this._inputQuantidade = $("#quantidade");
             this._inputValor = $("#valor");
-            this._listaNegociacoes = new ListaNegociacoes();
-            this._negociacoesView = new NegociacoesView($("#negociacoesView"));
 
-            this._negociacoesView.update(this._listaNegociacoes);
+            this._listaNegociacoes = new Bind(
+                new ListaNegociacoes(),
+                new NegociacoesView($("#negociacoesView")),
+                'adiciona','esvazia');
 
-            this._mensagem = new Mensagem();
-            this._mensagemView = new MensagemView($("#mensagemView"));
-            this._mensagemView.update(this._mensagem);
-
+            this._mensagem = new Bind(
+                new Mensagem(),
+                new MensagemView($("#mensagemView")),
+                'texto');          
     }
 
     adiciona(event){
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._negociacoesView.update(this._listaNegociacoes);
         this._mensagem.texto = 'Mensagem adicionada com sucesso!';
-        this._mensagemView.update(this._mensagem)
-
         this._limpaFormulario();
+    }
+
+    apaga(){
+        this._listaNegociacoes.esvazia();
+        this._mensagem.texto = 'Negociacao apagada com sucesso!';
     }
 
     _criaNegociacao(){
